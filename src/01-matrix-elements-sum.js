d79125e6ec7c84fdcba57ba4e291a603f11021b1
alexd1991 <a.dobrovolsky1991@gmail.com>
2021-04-08 22:03:29 +0300

    fix

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,10 +1,10 @@
 function getMatrixElementsSum(matrix) {
   let sum = 0;
-  const matrix1 = JSON.parse(JSON.stringify(matrix));
-  for (let row = 0; row < matrix1.length; row++) {
-    for (let col = 0; col < matrix1[row].length; col++) {
-      if (matrix1[row - 1] && matrix1[row - 1][col] === 0) matrix1[row][col] = 0;
-      sum += matrix1[row][col];
+  const matrixCopy = JSON.parse(JSON.stringify(matrix));
+  for (let row = 0; row < matrixCopy.length; row++) {
+    for (let col = 0; col < matrixCopy[row].length; col++) {
+      if (matrixCopy[row - 1] && matrixCopy[row - 1][col] === 0) matrixCopy[row][col] = 0;
+      sum += matrixCopy[row][col];
     }
   }
   return sum;

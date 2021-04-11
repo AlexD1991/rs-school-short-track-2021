19b191b7acd7b3803fdd890c929c8f63dad718ae
alexd1991 <a.dobrovolsky1991@gmail.com>
2021-04-11 17:18:48 +0300

    feat: mine sweeper task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,28 +1,17 @@
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const resultMatrix = matrix.slice().map((row) => row.map(() => 0));
+  for (let row = 0; row < resultMatrix.length; row++) {
+    for (let col = 0; col < resultMatrix[row].length; col++) {
+      for (let i = row - 1; i <= row + 1; i++) {
+        for (let j = col - 1; j <= col + 1; j++) {
+          if (!(i === row && j === col) && matrix[i] && matrix[i][j]) {
+            resultMatrix[row][col]++;
+          }
+        }
+      }
+    }
+  }
+  return resultMatrix;
 }
 
 module.exports = minesweeper;

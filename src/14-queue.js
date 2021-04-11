041a1954173339e878b5f90e0077376c307f57c0
alexd1991 <a.dobrovolsky1991@gmail.com>
2021-04-11 16:49:28 +0300

    feat: queue task

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,27 +1,52 @@
-// const ListNode = require('../extensions/list-node');
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- *
- */
+const ListNode = require('../extensions/list-node');
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.first = null;
+    this.last = null;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    if (this.last === null) return 0;
+    if (this.last === this.first) return 1;
+    let counter = 2;
+    let current = this.last;
+    while (current.next !== this.first) {
+      counter++;
+      current = current.next;
+    }
+    return counter;
+  }
+
+  enqueue(element) {
+    const node = new ListNode();
+    node.value = element;
+    if (this.size === 0) {
+      this.last = node;
+      this.first = node;
+      node.next = null;
+    } else {
+      node.next = this.last;
+      this.last = node;
+    }
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const resultValue = this.first.value;
+    if (this.size === 0) {
+      throw new Error();
+    } else if (this.size === 1) {
+      this.last = null;
+      this.first = null;
+    } else {
+      let current = this.last;
+      while (current.next !== this.first) {
+        current = current.next;
+      }
+      current.next = null;
+      this.first = current;
+    }
+    return resultValue;
   }
 }
 

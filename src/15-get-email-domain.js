ceae80009f5d67ea5b917b9174982b150ff05636
alexd1991 <a.dobrovolsky1991@gmail.com>
2021-04-11 16:55:18 +0300

    feat: domain part task

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -1,15 +1,5 @@
-/**
- * Given an email address, return it's domain.
- *
- * @param {String} email
- * @return {String}
- *
- * @example
- * For the input 'prettyandsimple@example.com', the output should be 'example.com'
- *
- */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  return email.substring(email.lastIndexOf('@') + 1, email.length);
 }
 
 module.exports = getEmailDomain;

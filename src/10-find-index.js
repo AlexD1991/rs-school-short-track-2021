4b78834196daa9726c4ef29c42ee5a31485b17d8
alexd1991 <a.dobrovolsky1991@gmail.com>
2021-04-11 13:46:07 +0300

    feat: binary search task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,18 +1,18 @@
-/**
- * Given a sorted array, find the index of the element with the given value.
- * Time complexity should be O(logN)
- *
- * @param {Array} array
- * @param {Number} value
- * @return {Number}
- *
- * @example
- * For ([1, 2, 3], 1) should return 0
- * For ([1, 2, 3], 2) should return 1
- *
- */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let first = 0;
+  let last = array.length - 1;
+  while (first <= last) {
+    const mid = Math.floor((first + last) / 2);
+    if (array[mid] === value) {
+      return mid;
+    }
+    if (array[mid] < value) {
+      first = mid + 1;
+    } else {
+      last = mid - 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = findIndex;

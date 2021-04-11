8e2aef07b823dc201c1cf4b75699ddd5c619aec4
alexd1991 <a.dobrovolsky1991@gmail.com>
2021-04-11 13:18:33 +0300

    feat: calculate sum of digits task

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,17 +1,12 @@
-/**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
- */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const sum = n
+    .toString()
+    .split('')
+    .reduce((calc, item) => calc + parseInt(item, 10), 0);
+  if (sum > 9) {
+    return getSumOfDigits(sum);
+  }
+  return sum;
 }
 
 module.exports = getSumOfDigits;

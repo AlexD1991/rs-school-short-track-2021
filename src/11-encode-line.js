d8dfc969406bb8e2c493955f39a2043e70eb0206
alexd1991 <a.dobrovolsky1991@gmail.com>
2021-04-11 14:13:27 +0300

    feat: encode line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,15 +1,19 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let encodedStr = '';
+  let cur = 0;
+  for (let i = 0; i < arr.length; i++) {
+    cur = i;
+    while (arr[i] === arr[cur]) {
+      cur++;
+    }
+    if (cur - i !== 1) {
+      encodedStr += cur - i;
+    }
+    encodedStr += arr[i];
+    i = cur - 1;
+  }
+  return encodedStr;
 }
 
 module.exports = encodeLine;
